6ba80ef59fa4b2896ae00ebd99e2a1ba53e74f0f
shrimpgood <145854081+shrimpgood@users.noreply.github.com>
2024-05-29 15:14:52 +0800

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp');
+const del = require("del");
+var ts = require('gulp-typescript');
+var runSequence = require("run-sequence");
+const browserSync = require('browser-sync');
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('clean', (cb) => {
+    return del(["build"], cb);
+});
+
+gulp.task('build',  function() {
+    gulp.src(["src/**/*", "!**/*.ts"]).pipe(gulp.dest("build"));
+    var tsResult = gulp.src("src/**/*.ts").pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest('build'));
+});
+
+gulp.task('libs', () => {
+    return gulp
+        .src([
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/jquery/dist/jquery.min.map',
+            'node_modules/systemjs/dist/system-polyfills.js',
+            'node_modules/systemjs/dist/system-polyfills.js.map',
+            'node_modules/systemjs/dist/system.js',
+            'node_modules/systemjs/dist/system.js.map',
+            'node_modules/pixi.js/dist/pixi.min.js',
+            'node_modules/pixi.js/dist/pixi.min.js.map',
+            'node_modules/howler/dist/howler.core.min.js',
+            'node_modules/gsap/src/minified/TweenLite.min.js',
+            'node_modules/gsap/src/minified/TimelineMax.min.js '
+        ])
+        .pipe(gulp.dest("build/lib"));
+});
+
+gulp.task('watch-web', () => {
+    gulp.watch(["src/**/*"], ['build']);
+})
+
+gulp.task("build-web", function () {
+    runSequence(
+        'watch-web',
+        'clean',
+        ['build', 'libs'],
+        function (error) {
+            if (error) {
+                console.log(error.message);
+            } else {
+                console.log('success');
+            }
+        }
+    );
+});
+
+gulp.task("default", ['launch-web'], function () {
+});
+
+gulp.task('launch-web', ['build-web'], function () {
+    browserSync({
+        open: true,
+        port: 8001,
+        files: ["./build/**/*.{html,htm,css,js,json}"],
+        server: {
+            "baseDir": "./build"
+        }
+    });
+});
